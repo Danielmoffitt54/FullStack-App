137ba32e5ef90d021735510e40e0a79a6f9283ea
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-11-25 10:12:02 -0600

    Added in gitignore to ignore node modules, installed packages for server

--- backend/secret.js
+++
@@ -1,8 +0,0 @@
-const secrets = {
-    // The URL that we use to connect to the MongoDB Atlas Cluster.
-    dbUrl: 'mongodb+srv://MoffittD:<password>@cluster0-7kivy.mongodb.net/test?retryWrites=true&w=majority'
-};
-
-const getSecret = key => secrets[key];
-
-module.exports = getSecret;
