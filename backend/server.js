3d121caad0029056048cc5c565512e4cd7fb3ca8
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-12-02 12:17:21 -0600

    Added the ability to get data and add data to the database from the server, and set up the frontend to carry out those operations.

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 // Import the getSecret function from the secret.js file.
 const getSecret = require('./secret');
+const Data = require('./DataSchema');
 
 // Constant to hold the port that we are going to use to connect.
 const API_PORT = 3001;
@@ -32,13 +33,55 @@ app.use(bodyParser.urlencoded({ extended: false })); // Used for parsing data fo
 app.use(bodyParser.json()); // Used for parsing data formatted in the JSON format.
 app.use(logger('dev'));
 
-// Default route that will run whenever we first connect to the server.
+// Default rotue that will retrieve data from the database.
 router.get('/', (req, res) => {
-    res.json({ message: 'HELLO WORLD' });
+    res.json({ message: 'HELLO WORLD'});
+});
+
+// Default route that will run whenever we first connect to the server.
+router.get('/getData', (req, res) => {
+    // Use mongoose to find data with the given  schema.
+    Data.find((err, data) => {
+        console.log(data);
+
+        if (err) {
+            return res.json({ success : false, error: err });
+        }
+
+        return res.json({ success : true, error: data });
+    });
+});
+
+// Route that will put new information in the database.
+router.post('/postData', (req, res) => {
+    // A new instance of the Mongoose Schema which we will send to the database.
+    let newData = new Data();
+
+    // Pull the ID and message from the body of the request.
+    const { id, message } = req.body;
+
+    if ((!id && id !== 0) || !message) {
+        return res.json({
+            success: false,
+            error: 'INVALID INPUT'
+        })
+    }
+
+    newData.id;
+    newData.message;
+
+    newData.save(err => {
+        if (err) {
+            return res.json({ success: false, error: err 
+            });
+        }
+
+        return res.json({ success: true });
+    });
 });
 
 // Tell Express to use a certain path and to use the router we set up.
-app.use('/', router);
+app.use('/api', router);
 
 // Tell Express to listen for requests on the appropriate port.
 app.listen(API_PORT, () => console.log('LISTENING ON PORT:' + API_PORT));
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,114 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import { set } from 'mongoose';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      message: null,
+      idToDelete: null,
+      idToUpdate: null,
+      newMessage: null
+    }
+  }
+
+  componentDidMount() {
+    this.getDataFromDB();
+    // Set up and interval to call the getdDataFromDB function every second.
+    setInterval(this.getDataFromDB, 1000);
+  }
+
+  getDataFromDB = () => {
+    axios({
+      url: 'http://localhost:3001/api/getData',
+      method: 'GET'
+    }).then((response) => {
+      console.log(response);
+      // Update our response with the data from the backend.
+      this.setState({ data: response.data.data })
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  // Made function as an arrow function to negate the binding of the "this" keyword.
+  postDataToDB = message => {
+    // 1. Figure out what ID this message needs to have.
+    // 2. Use Axios to connect to our API server, which will send the data to our database.
+
+    let currentIds = this.state.data.map(data => data.id);
+    let idToBeAdded = 0;
+    while (currentIds.includes(idToBeAdded)) {
+      idToBeAdded++;
+    }
+
+    axios({
+      url: 'http://localhost:3001/api/postData',
+      method: 'POST',
+      data: {
+        id: idToBeAdded,
+        message: message
+      }
+    }).then((reponse) => {
+      console.log(reponse);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  renderListItems() {
+    const { data } = this.state;
+
+    if (data.length === 0) {
+      // If our data array doesn't have any contents, display a message letting the user know that the database doesn't have anything inside it yet.
+      return 'No DB ENTRIES YET'
+    } else {
+      return data.map(dat => (
+        <li key={dat}>
+          <span> id: {dat.id} </span>
+          <br />
+          <span> message: {dat.message}</span>
+        </li>
+      ));
+    }
+  }
+
+  render() {
+    const { data, message, intervalIsSet, idToDelete, idToUpdate, newMessage } = this.state;
+
+    return (
+      <div className="App">
+
+        {/* Disply the data we retrieve from the database */}
+        <ul className='App-List'>
+          {this.renderListItems()}
+        </ul>
+
+        <div className='App-section'>
+          <input 
+            type='text'
+            placeholder='Add a New Message to the database'
+            onChange={event => this.setState({ message: event.target.value})}
+          />
+          <button onClick={() => this.postDataToDB(message)}>ADD</button>
+        </div>
+
+        <div className='App-section'>
+          <input />
+          <button>DELETE</button>
+        </div>
+
+        <div className='App-section'>
+          <input />
+          <input />
+          <button>UPDATE</button>
+        </div>
+
+      </div>
+    );
+  }
 }
-
-export default App;
